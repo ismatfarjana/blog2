2464628559db17645d976dc630e2a2c33582fcbb
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2020-06-12 00:44:14 +1000

    create, new, form, back, edit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,39 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def show
+
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article, notice: 'Article was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+
+  end
+
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :body, :author)
+  end
+
+end
